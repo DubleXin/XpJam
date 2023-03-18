2cb689cdac74b1d7fb4e7076f838006d68ce0c38
Kirserd <87417121+Kirserd@users.noreply.github.com>
2023-03-18 12:18:44 +0100

    Patrol AI half done

--- XpJam/Assets/Scripts/Enemy/PatrolAI.cs
+++ XpJam/Assets/Scripts/Enemy/PatrolAI.cs
@@ -1,15 +1,62 @@
 using UnityEngine;
+using System.Collections;
 
 public class PatrolAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum PatrolState
     {
-        
+        PATROL,
+        PURSUIT
     }
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float _visionRadius;
+    [SerializeField] private Vector2[] _patrolDestinations;
+    private int _currentDestination = 0;
+    private PatrolState _state = PatrolState.PATROL;
+    private TopDownMovement _movement;
+    private void Start()
+    {
+        _movement = GetComponent<TopDownMovement>();
+        StartCoroutine(SlowUpdate());
+    }
+    private IEnumerator SlowUpdate()
+    {
+        while (true) 
+        {
+            if (_state == PatrolState.PATROL && Physics2D.CircleCastAll(Vector2.zero,
+                _visionRadius, Vector2.zero, 0, LayerMask.NameToLayer("Creature")).Length > 0)
+            {
+                _state = PatrolState.PURSUIT;
+            }
+            else if (_state == PatrolState.PURSUIT && Physics2D.CircleCastAll(Vector2.zero,
+                _visionRadius, Vector2.zero, 0, LayerMask.NameToLayer("Creature")).Length == 0) 
+            {
+                _state = PatrolState.PATROL;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_state == PatrolState.PATROL) Patrol();
+        else Pursuit();
+    }
+    private void Patrol()
+    {
+        if (_currentDestination < _patrolDestinations.Length)
+        {
+            if (Vector2.Distance(transform.position, _patrolDestinations[_currentDestination]) > 1f)
+            {
+                _movement.OnMove(new Vector2
+                (
+                    _patrolDestinations[_currentDestination].x - transform.position.x,
+                    _patrolDestinations[_currentDestination].y - transform.position.y)
+                );
+            }
+            else _currentDestination++;
+        }
+        else _currentDestination = 0;
+    }
+    private void Pursuit() 
     {
         
     }
